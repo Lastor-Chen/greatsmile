2eba9e659526de5bc4128cbbf1f4ea7790ca0a6e
Lastor <lastoraya@gmail.com>
2019-12-16 18:13:27 +0800

    [調整] sequelize config 屏蔽敏感資訊

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
-{
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql",
     "logging": false
   },
