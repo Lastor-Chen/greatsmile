b6f3e511bb74ac32665bc4c9d42f6823f84f79d1
TomatoSoup <jonathantang0126@gmail.com>
2019-12-19 01:51:02 +0800

    [新增] 註冊頁面, 註冊, 登入頁面, 登入, 登出等user controller功能實作

--- controllers/userCtrller.js
+++ controllers/userCtrller.js
@@ -1,21 +1,53 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+
+const { checkSignUp } = require('../lib/checker.js')
+
 module.exports = {
   getSignUp: (req, res) => {
-    res.send('sign up')
+    res.render('signup')
   },
 
-  signUp: (req, res) => {
-    res.send('POST sign up')
+  signUp: async (req, res) => {
+    try {
+      const input = { ...req.body }  // 深拷貝，保護原始資料
+
+      // check input
+      const error = await checkSignUp(input)
+      if (error) return res.render('signup', { error, input })
+
+      input.avatar = '/img/default.png'
+      input.introduction = ''
+      input.password = bcrypt.hashSync(input.password, 10)
+      await User.create(input)
+
+      req.flash('success', '已成功註冊帳號')
+      res.redirect('/signin')
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ status: 'serverError', message: err.toString() })
+    }
   },
 
   getSignIn: (req, res) => {
-    res.send('sign in')
+    return res.render('signin')
   },
 
-  signIn: (req, res) => {
-    res.send('POST sign in')
+  signIn: (passport, req, res, next) => {
+    passport.authenticate('local', {
+      successRedirect: '/admin',
+      successFlash: true,
+      failureRedirect: '/signin',
+      failureFlash: true,
+      badRequestMessage: '請輸入 Email 與 Passport'
+    })(req, res, next)
   },
 
   signOut: (req, res) => {
-    res.send('sign out')
+    req.flash('success', '登出成功！')
+    req.logout()
+    res.redirect('/')
   },
 }
