62e976d3d6b9d3c2b83f1c378049ee0a176fb7f3
TomatoSoup <jonathantang0126@gmail.com>
2019-12-19 01:47:38 +0800

    [新增] 設定passport

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,36 @@
 const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
 const db = require('../models')
+const User = db.User
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+
+  (email, password, done) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) return done(null, false, { message: '帳號或密碼輸入錯誤' })
+        if (!bcrypt.compareSync(password, user.password)) return done(null, false, { message: '帳號或密碼輸入錯誤！' })
+
+        done(null, user, { message: '登入成功' })
+      })
+      .catch(err => console.error(err))
+  }
+))
+
+// serialize and deserialize user
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+passport.deserializeUser((id, done) => {
+  User.findByPk(id, {
+    include: [{ all: true, nested: false }]
+  })
+    .then(user => done(null, user))
+})
 
 module.exports = passport
