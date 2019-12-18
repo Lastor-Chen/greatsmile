445b6bcd33fb7252754fb4afea23c65f3ef7fb86
wendy hsiao <wendy19940331@gmail.com>
2019-12-18 21:35:59 +0800

    [修改] Product 的內容調整

--- seeders/20191218080007-product-seeder.js
+++ seeders/20191218080007-product-seeder.js
@@ -19,10 +19,10 @@ module.exports = {
          maker: faker.company.companyName(),
          status: 1,
          release_date: faker.date.between('2019-10-01', '2019-10-31'), 
-         sale_date: index < 6 ? faker.date.between('2019-12-01', '2019-12-31') : faker.date.between('2019-02-01', '2019-02-31'),
+         sale_date: index < 6 ? faker.date.between('2019-12-01', '2019-12-18') : faker.date.between('2019-02-01', '2019-02-31'),
          deadline: index < 11 ? faker.date.between('2019-11-01', '2019-11-31') : faker.date.between('2019-01-01', '2019-01-31'),
-         Series_id: randomNum(1, 5),
-         Category_id: randomNum(1, 5)
+         Series_id: randomNum(1, 4),
+         Category_id: randomNum(1, 4)
        }))
      )
   }, 
@@ -35,6 +35,6 @@ module.exports = {
 
 
   // release_date: 1-20 項商品在10月份公開
-  // deadline:     1-10 在11月截止預約, 11-20 可預約至明年 1 月
-  // sale_date:    1-5  在12月開始販售, 6-20 將在明年 2 月份銷售,
+  // deadline:     1-10 在11月截止預約, 11-20 可預約至明年 1 月(預約中)
+  // sale_date:    1-5  在12月開始販售, 6-20 將在明年 2 月份販售,
   //               6-10 等待販售(下架狀態)
