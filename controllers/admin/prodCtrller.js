822077b51a1ba0439d938ac8efcc0ebbd7c4ecbf
TomatoSoup <jonathantang0126@gmail.com>
2019-12-21 00:59:23 +0800

    [加入] 取得商品資訊渲染至表格中

--- controllers/admin/prodCtrller.js
+++ controllers/admin/prodCtrller.js
@@ -1,5 +1,27 @@
+const db = require('../../models')
+const { Product, Category, Series, Image } = db
+
 module.exports = {
-  getProducts: (req, res) => {
-    res.send('admin products')
+  getProducts: async (req, res) => {
+    try {
+      const [products] = await Promise.all([
+        Product.findAll({
+          order: [['id', 'DESC']],
+          include: [Category, Series, Image]
+        })
+      ])
+
+      products.forEach(product => {
+        if (product.Images.length != 0) {
+          product.imageUrl = product.Images[0].url
+        } else {
+          product.imageUrl = 'https://citainsp.org/wp-content/uploads/2016/01/default.jpg'
+        }
+      })
+      res.render('admin/products', { products })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json(err.toString())
+    }
   }
 }
