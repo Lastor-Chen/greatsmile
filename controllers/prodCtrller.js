49b39318100b1f3b08d097b1f411ff56310af499
Lastor <lastoraya@gmail.com>
2019-12-20 15:40:17 +0800

    [加入] 使 product 頁面，串接後端資料

--- controllers/prodCtrller.js
+++ controllers/prodCtrller.js
@@ -1,9 +1,27 @@
+const db = require('../models')
+const Product = db.Product
+
 module.exports = {
   getProducts: (req, res) => {
     res.send('products')
   },
 
-  getProduct: (req, res) => {
-    res.render('product')
+  getProduct: async (req, res) => {
+    try {
+      const product = await Product.findByPk(+req.params.id, { 
+        include: ['Gifts', 'Images', 'tags', 'Series'] 
+      })
+      console.log(product.dataValues)
+      product.mainImg = product.Images[0].url
+      product.release = product.releaseDate.toLocaleDateString()
+      product.dead = product.deadline.toLocaleDateString()
+      product.hasGift = product.Gifts.length !== 0 ? true : false
+
+      res.render('product', { product })
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ status: 'serverError', message: err.toString() })
+    }
   }
 }
