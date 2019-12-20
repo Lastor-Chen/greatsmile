30da6c789bdfe8738db4420f928ec337c0a1e265
TomatoSoup <jonathantang0126@gmail.com>
2019-12-21 01:54:53 +0800

    [加入] 加入發售日至表格中

--- controllers/admin/prodCtrller.js
+++ controllers/admin/prodCtrller.js
@@ -1,6 +1,8 @@
 const db = require('../../models')
 const { Product, Category, Series, Image } = db
 
+const moment = require('moment')
+
 module.exports = {
   getProducts: async (req, res) => {
     try {
@@ -12,12 +14,18 @@ module.exports = {
       ])
 
       products.forEach(product => {
+        //寫入第一筆圖片, 若無商品圖則寫入無圖片圖示
         if (product.Images.length != 0) {
           product.imageUrl = product.Images[0].url
         } else {
           product.imageUrl = 'https://citainsp.org/wp-content/uploads/2016/01/default.jpg'
         }
+
+        //格式化日期
+        product.simplifySale = moment(product.saleDate).format('YYYY/MM/DD')
+
       })
+      console.log(products[0])
       res.render('admin/products', { products })
     } catch (err) {
       console.error(err)
