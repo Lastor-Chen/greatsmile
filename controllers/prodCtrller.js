b160938479c09e45da9164da2a6fdd759685d5c6
Lastor <lastoraya@gmail.com>
2019-12-20 16:19:49 +0800

    [加入] 套件 momentjs，處理 product 頁日期顯示

--- controllers/prodCtrller.js
+++ controllers/prodCtrller.js
@@ -1,6 +1,9 @@
 const db = require('../models')
 const Product = db.Product
 
+const moment = require('moment')
+moment.locale('zh-tw')
+
 module.exports = {
   getProducts: (req, res) => {
     res.send('products')
@@ -11,10 +14,10 @@ module.exports = {
       const product = await Product.findByPk(+req.params.id, { 
         include: ['Gifts', 'Images', 'tags', 'Series'] 
       })
-      console.log(product.dataValues)
       product.mainImg = product.Images[0].url
-      product.release = product.releaseDate.toLocaleDateString()
-      product.dead = product.deadline.toLocaleDateString()
+      product.saleDateFormat = moment(product.saleDate).format('YYYY年MM月')
+      product.releaseDateFormat = moment(product.releaseDate).format('YYYY年MM月DD日(dd)')
+      product.deadlineFormat = moment(product.deadline).format('YYYY年MM月DD日(dd)')
       product.hasGift = product.Gifts.length !== 0 ? true : false
 
       res.render('product', { product })
