abc6a698d213c5042e0ad2f621ef2896cd36784e
wendy hsiao <wendy19940331@gmail.com>
2019-12-21 17:06:06 +0800

    [加入] getProducts 傳入資料至 products 頁面

--- controllers/prodCtrller.js
+++ controllers/prodCtrller.js
@@ -1,5 +1,27 @@
+const db = require('../models')
+const { Product } = db
+const moment = require('moment')
+
 module.exports = {
-  getProducts: (req, res) => {
-    res.send('products')
-  }
+  getProducts: async(req, res) => {
+    try{
+      const products = await Product.findAll({
+        include: ['Images', 'Gifts'],
+        order: [['saleDate', 'DESC']]
+      })
+
+      const today = new Date()
+      products.forEach(product => {
+        product.firstimage = product.Images[0]
+        product.isPreorder = moment(today).isBefore(product.deadline)
+        product.isGift = product.Gifts.length > 0 ? true :false
+      });  
+
+      return res.render('products', { products, css: 'products' })
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ status: 'serverError', message: err.toString() })
+    }
+  },
 }
