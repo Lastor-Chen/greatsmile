ab1c3f46b4e2d5fc49670139b6a6dca0c8fa485f
TomatoSoup <jonathantang0126@gmail.com>
2019-12-24 01:21:36 +0800

    [加入] 加入中間件切換admin頁面的layout

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -3,8 +3,16 @@ const prodCtrller = require('../../controllers/admin/prodCtrller.js')
 
 const { isAdminAuth } = require('../../middleware/auth')
 
+
+
 // route base '/admin'
 router.use('/', isAdminAuth)
+// set admin layout
+router.use('/', (req, res, next) => {
+  res.locals.layout = 'admin'
+  next()
+})
+
 router.get('/', (req, res) => res.redirect('/admin/products'))
 router.get('/products', prodCtrller.getProducts)
 router.get('/products/new', prodCtrller.getAddPage)
