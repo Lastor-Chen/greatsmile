4e9dd794d0fa1d2104e7c5905ab1d72aba4f9347
TomatoSoup <jonathantang0126@gmail.com>
2019-12-24 02:19:42 +0800

    [加入] Image model加入isMain屬性

--- models/image.js
+++ models/image.js
@@ -2,9 +2,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Image = sequelize.define('Image', {
     url: DataTypes.STRING,
-    ProductId: DataTypes.INTEGER
+    ProductId: DataTypes.INTEGER,
+    isMain: DataTypes.BOOLEAN
   }, {});
-  Image.associate = function(models) {
+  Image.associate = function (models) {
     Image.belongsTo(models.Product)
   };
   return Image;
