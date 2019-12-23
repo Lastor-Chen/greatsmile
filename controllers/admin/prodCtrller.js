198f3c6bbb5799ed6d92be463a45c4fa92e6e253
TomatoSoup <jonathantang0126@gmail.com>
2019-12-24 02:20:58 +0800

    [優化] 更新內容邏輯, 寫入主商品圖, 售價加上dot

--- controllers/admin/prodCtrller.js
+++ controllers/admin/prodCtrller.js
@@ -6,31 +6,32 @@ const moment = require('moment')
 module.exports = {
   getProducts: async (req, res) => {
     try {
-      const [products] = await Promise.all([
-        Product.findAll({
-          order: [['id', 'DESC']],
-          include: [Category, Series, Image]
-        })
-      ])
+      const products = await Product.findAll({
+        order: [['id', 'DESC']],
+        include: [Category, Series, Image]
+      })
+
 
       //判斷日期用
       const today = new Date()
 
       products.forEach(product => {
-        //寫入第一筆圖片, 若無商品圖則寫入無圖片圖示
+        //寫入主商品圖, 若無商品圖則寫入無圖片圖示
         if (product.Images.length != 0) {
-          product.imageUrl = product.Images[0].url
+          product.mainImg = product.Images.find(img => img.isMain).url
         } else {
-          product.imageUrl = 'https://citainsp.org/wp-content/uploads/2016/01/default.jpg'
+          product.mainImg = 'https://citainsp.org/wp-content/uploads/2016/01/default.jpg'
         }
 
         //判斷發售狀態
-        if (product.saleDate.valueOf() > today.valueOf()) {
-          product.saleStatus = '未發售'
+        if (moment(today).isBefore(product.saleDate)) {
+          product.saleStatus = '預約中'
         } else {
           product.saleStatus = '已發售'
         }
 
+        //售價加上dot
+        product.price = product.price.toLocaleString()
       })
       res.render('admin/products', { products, css: 'admin' })
     } catch (err) {
