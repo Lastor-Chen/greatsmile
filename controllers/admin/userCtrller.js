1ed7a4825cfb3d16aae7d40c6e7caed468a6d53e
TomatoSoup <jonathantang0126@gmail.com>
2019-12-27 16:54:00 +0800

    [加入] 傳入users至user.hbs渲染會員資訊

--- controllers/admin/userCtrller.js
+++ controllers/admin/userCtrller.js
@@ -5,8 +5,16 @@ const { User } = db
 module.exports = {
   getUsers: async (req, res) => {
     try {
+      const users = await User.findAll({
+        order: [['id', 'DESC']]
+      })
 
-      res.render('admin/users', { css: 'admin' })
+      users.forEach(user => {
+        user.modifybirthday = user.birthday.toJSON().split('T')[0]
+      })
+
+      console.log(users)
+      res.render('admin/users', { users, css: 'admin' })
     } catch (err) {
       console.error(err)
       res.status(500).json(err.toString())
