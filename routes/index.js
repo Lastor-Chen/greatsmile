6026fa6ad3b24920324ff6644f2b93080df66546
Lastor <lastoraya@gmail.com>
2019-12-27 15:46:58 +0800

    [debug] 使前台全頁面，皆能正確顯示購物車計數

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,16 @@
  * 路由索引頁
  * 依 RESTful 風設計，再細拆成 router
  */
+const { cartItemSum } = require('../middleware/cart.js')
 
 module.exports = app => {
-  app.get('/', (req, res) => res.render('home'))
+  app.use('/admin', require('./admin/admin.js'))
 
+  app.use('/', cartItemSum)
   app.use('/products', require('./products.js'))
   app.use('/cart', require('./cart.js'))
   app.use('/users', require('./users.js'))
-  app.use('/admin', require('./admin/admin.js'))
-
+  
+  app.get('/', (req, res) => res.render('home'))
   app.get('/search', require('../controllers/prodCtrller').getProducts)
 }
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,6 @@
 const router = require('express').Router()
 const prodCtrller = require('../controllers/prodCtrller.js')
 
-const { cartItemSum } = require('../middleware/cart.js')
-
-router.use('/', cartItemSum)
-
 // route base '/products'
 router.get('/', prodCtrller.getProducts)
 router.get('/:id', prodCtrller.getProduct)
