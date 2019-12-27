83e7d275cb093bd3ef2e55b3ff5ab249ea644189
Lastor <lastoraya@gmail.com>
2019-12-27 13:51:02 +0800

    [加入] postCart 限制單一購買數量為 3 筆

--- controllers/cartCtrller.js
+++ controllers/cartCtrller.js
@@ -5,22 +5,31 @@ const CartItem = db.CartItem
 module.exports = {
   async postCart(req, res) {
     try {
+      const QTY_Limit = 3
+
       const cartId = req.session.cartId || null
       const product_id = req.body.productId
+      const inputQty = +req.body.quantity || 1
 
-      // 確認 client 是否已有 cart，若無就給一個
+      // 確認 client 是否已有 Cart，若無就給一個
       const [cart] = await Cart.findOrCreate({ where: { id: cartId } })
-
-      // 確認 cart 是否已有 item，若無就給一個
-      const [cartItem] = await CartItem.findOrCreate({ where: { CartId: cart.id, product_id } })
-
-      // 遞增目標商品數量
-      await cartItem.increment('quantity')
 
       // 發 session 紀錄用戶是哪台 cart
       req.session.cartId = cart.id
       await req.session.save()
 
+      // 確認 CartItem 是否已有 record，若無建一個
+      const [cartItem] = await CartItem.findOrCreate({ where: { CartId: cart.id, product_id } })
+      const currentQty = cartItem.quantity || 0
+
+      // 計算商品數量
+      if (currentQty + inputQty > QTY_Limit) {
+        console.log('不能超過 3 個')
+        await cartItem.update({ quantity: QTY_Limit })
+        return res.redirect('back')
+      } 
+      
+      await cartItem.update({ quantity: (currentQty + inputQty) })
       res.redirect('back')
 
     } catch (err) {
