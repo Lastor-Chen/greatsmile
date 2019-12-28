fb48e8f90ed5aca02cb4a0f9d752cf40f6218914
TomatoSoup <jonathantang0126@gmail.com>
2019-12-28 23:12:12 +0800

    [加入] Order seeder

--- migrations/20191228134628-create-order.js
+++ migrations/20191228134628-create-order.js
@@ -37,11 +37,13 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
       }
     });
   },
