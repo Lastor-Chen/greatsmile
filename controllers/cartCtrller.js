0b85c1a7f06d787c5679cb17f3979ef9d4e43589
wendy hsiao <wendy19940331@gmail.com>
2019-12-28 19:18:31 +0800

    [加入] getCart 傳入資料至 cart 頁面

--- controllers/cartCtrller.js
+++ controllers/cartCtrller.js
@@ -1,7 +1,39 @@
 const db = require('../models')
+const Product = db.Product
+const Cart = db.Cart
+const CartItem = db.CartItem
+
+const moment = require('moment')
+moment.locale('zh-tw')
 
 module.exports = {
-  getCart: (req, res) => {
-    res.render('cart', { css: 'cart' })
+  getCart: async(req, res) => {
+    try {
+      const cart = await Cart.findOne({ 
+        include: [{ model: Product, as: 'products', 
+                    include: ['Gifts', 'Images']}]
+      })
+      
+      let cartProducts = cart.products
+      let totalPrice = 0
+      if (cartProducts.length > 0) {
+        const today = new Date()
+        cartProducts.forEach(product => {
+          product.priceFormat = product.price.toLocaleString()
+          product.subPriceFormat = (product.price * product.CartItem.quantity).toLocaleString()
+          product.mainImg = product.Images.find(img => img.isMain).url
+          product.isPreorder = moment(today).isBefore(product.deadline)
+          product.isGift = product.Gifts.length > 0 ? true : false
+          totalPrice += (product.price * product.CartItem.quantity)
+        } 
+      )}
+      const totalPriceFormat = totalPrice.toLocaleString()
+      
+      res.render('cart', { cartProducts, totalPriceFormat, css: 'cart' })
+
+    } catch(err) {
+      console.error(err)
+      res.status(500).json({ status: 'serverError', message: err.toString() })
+    }
   }
 }
