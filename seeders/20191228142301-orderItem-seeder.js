9036dd9363612d91b2c0c3426735a858ab357a4a
TomatoSoup <jonathantang0126@gmail.com>
2019-12-29 17:41:00 +0800

    [加入] OrderItem seeder

--- seeders/20191228142301-orderItem-seeder.js
+++ seeders/20191228142301-orderItem-seeder.js
@@ -1,26 +1,23 @@
 'use strict';
 
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('OrderItems',
+      Array.from({ length: 30 }, (val, index) => ({
+        price: randomNum(1000, 10000),
+        quantity: randomNum(1, 3),
+        order_id: randomNum(1, 25),
+        product_id: randomNum(1, 100)
+      }))
+    )
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    const option = { truncate: true, restartIdentity: true }
+    return queryInterface.bulkDelete('OrderItems', null, option)
   }
 };
