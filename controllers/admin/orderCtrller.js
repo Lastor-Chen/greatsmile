750570e3d5aa6b931bec755bbb0e07caebcbacc0
TomatoSoup <jonathantang0126@gmail.com>
2019-12-31 15:36:25 +0800

    [加入] order表格帶入user資訊

--- controllers/admin/orderCtrller.js
+++ controllers/admin/orderCtrller.js
@@ -1,9 +1,24 @@
 const db = require('../../models')
-const { Order } = db
+const { Order, User } = db
 
 
 module.exports = {
-  getOrders: (req, res) => {
-    res.send('order page')
+  getOrders: async (req, res) => {
+    try {
+      const orders = await Order.findAll({
+        order: [['id', 'DESC']],
+        include: [User]
+      })
+
+      orders.forEach(order => {
+        order.createdTime = order.createdAt.toJSON().split('T')[0]
+      })
+
+      console.log(orders[0])
+      res.render('admin/orders', { orders })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ status: 'serverError', message: err.toString() })
+    }
   },
 }
