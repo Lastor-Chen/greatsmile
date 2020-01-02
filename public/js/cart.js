201f5a46156ca2990bfbdfa624ec9d21d8dc711d
Lastor <lastoraya@gmail.com>
2020-01-02 20:16:22 +0800

    [重構] cart 頁，前端 JS 寫法優化

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,36 +1,39 @@
-const index = $('.productQty').length
-for (let i = 0; i < index; i++) {
+// 掛上 input 加減按鈕功能
+$('.cart-product').on('click', e => {
+  if (e.target.matches('.btn-add')) {
+    const btnAdd = $(e.target).prev()
 
-  // input 的數量大於 3 會出現提醒字樣，數量也會改為3
-  $(`.productQty:eq(${i})`).bind('input propertychange', function () {
-    const quantity = $(`.productQty:eq(${i})`).val()
+    const quantity = +btnAdd.val()
+    btnAdd.val(quantity + 1)
+
+    btnAdd.change()  // 手動觸發 input change
+  }
+
+  if (e.target.matches('.btn-sub')) {
+    const btnSub = $(e.target).next()
+
+    const quantity = +btnSub.val()
+    btnSub.val(quantity - 1)
+
+    btnSub.change()
+  }
+})
+
+// 監控 input 商品數量
+$('.cart-product').on('change', e => {
+  if (e.target.matches('.productQty')) {
+    const quantity = +e.target.value
+    const note = $(e.target).parents('.note-flag').next()
+
+    // 不得小於 0
+    if (quantity < 0) return e.target.value = 0
+
+    // 不得大於 3
     if (quantity > 3) {
-      $(`.note:eq(${i})`).text('* 不可超過3樣')
-      $(`.productQty:eq(${i})`).val(3)
-    } else {
-      $(`.note:eq(${i})`).text('')
-    }
-  })
-
-  // 增加的數量點超過第三下會出現提醒字樣
-  $(`.btn-add:eq(${i})`).on('click', function() {
-    const quantity = $(`.productQty:eq(${i})`).val()
-    if (quantity == 3 ) {
-      $(`.note:eq(${i})`).text('* 不可超過3樣')
-      return
+      e.target.value = 3
+      return note.text('* 不可超過3樣')
     }
 
-    $(`.productQty:eq(${i})`).val(+quantity + 1)
-  })
-
-  // 減少的數量等於 0 將不能再往下減
-  $(`.btn-sub:eq(${i})`).on('click', function () {
-    const quantity = $(`.productQty:eq(${i})`).val()
-    if (quantity == 0) {
-      return
-    }
-    $(`.note:eq(${i})`).text('')
-    $(`.productQty:eq(${i})`).val(+quantity - 1)
-  })
-}
-
+    note.text('')
+  }
+})
