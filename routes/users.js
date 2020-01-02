9714f44109460f207ece2630aa794b89586b77ed
TomatoSoup <jonathantang0126@gmail.com>
2020-01-02 15:31:26 +0800

    [加入] profile route加入使用者認證中間件

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const userCtrller = require('../controllers/userCtrller')
+const { isAuth } = require('../middleware/auth')
 
 // route base '/users'
 router.get('/', (req, res) => res.send('users page'))
@@ -8,6 +9,7 @@ router.post('/signup', userCtrller.signUp)
 router.get('/signin', userCtrller.getSignIn)
 router.post('/signin', userCtrller.signIn)
 router.get('/signout', userCtrller.signOut)
-router.get('/profile', userCtrller.getProfile)
+
+router.get('/profile', isAuth, userCtrller.getProfile)
 
 module.exports = router
