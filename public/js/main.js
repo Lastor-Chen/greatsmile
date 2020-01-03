c5e74d9ec6bc4d8b1527646b98870b8bf1b001b9
TomatoSoup <jonathantang0126@gmail.com>
2020-01-03 18:11:55 +0800

    [優化] 改採sticky css處理該效果

--- public/js/main.js
+++ public/js/main.js
@@ -17,15 +17,3 @@ if ($('.cart-alert').length) {
     $('.cart-alert').alert('close')
   }, 5000)
 }
-
-// sticky效果
-window.onscroll = function () { myFunction() };
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
