47c609d67bf817013a97e49af492a3d18407f364
TomatoSoup <jonathantang0126@gmail.com>
2020-01-03 17:42:03 +0800

    [加入] 新增、編輯Tag功能

--- controllers/admin/tagCtrller.js
+++ controllers/admin/tagCtrller.js
@@ -16,4 +16,58 @@ module.exports = {
       res.status(500).json(err.toString())
     }
   },
+
+  postTags: async (req, res) => {
+    try {
+      const input = { ...req.body }
+
+      await Tag.create(input)
+
+      res.redirect('/admin/tags')
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json(err.toString())
+    }
+  },
+
+  getEditPage: async (req, res) => {
+    try {
+
+      const id = req.params.tagsid
+      const tag = await Tag.findByPk(id)
+
+      const tags = await Tag.findAll({
+        order: [['id', 'DESC']]
+      })
+
+      res.render('admin/tags', { tag, tags })
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json(err.toString())
+    }
+  },
+
+  putTag: async (req, res) => {
+    try {
+
+      const input = { ...req.body }
+      const id = req.params.tagsid
+      const tag = await Tag.findByPk(id)
+
+      await tag.update(input)
+
+      const tags = await Tag.findAll({
+        order: [['id', 'DESC']]
+      })
+
+      res.render('admin/tags', { tags })
+
+    } catch (err) {
+      console.error(err)
+      res.status(500).json(err.toString())
+    }
+  },
+
 }
--- routes/admin/tags.js
+++ routes/admin/tags.js
@@ -3,5 +3,8 @@ const tagCtrller = require('../../controllers/admin/tagCtrller')
 
 // route base '/admin/tags'
 router.get('/', tagCtrller.getTags)
+router.post('/', tagCtrller.postTags)
+router.get('/:tagsid', tagCtrller.getEditPage)
+router.put('/:tagsid', tagCtrller.putTag)
 
 module.exports = router
