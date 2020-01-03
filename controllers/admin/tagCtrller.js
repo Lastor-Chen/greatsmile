c14a93ce97f43f2627de183072236556f7ec23d7
TomatoSoup <jonathantang0126@gmail.com>
2020-01-03 17:04:31 +0800

    [加入] GET Tags頁面

--- controllers/admin/tagCtrller.js
+++ controllers/admin/tagCtrller.js
@@ -1,12 +1,16 @@
 const db = require('../../models')
-const { User } = db
+const { Tag } = db
 
 
 module.exports = {
   getTags: async (req, res) => {
     try {
 
-      res.render('admin/tags')
+      const tags = await Tag.findAll({
+        order: [['id', 'DESC']]
+      })
+
+      res.render('admin/tags', { tags })
     } catch (err) {
       console.error(err)
       res.status(500).json(err.toString())
