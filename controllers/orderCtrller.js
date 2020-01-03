4ab21875ab3c4ca8977dfa7a2f0d627b04b8948f
Lastor <lastoraya@gmail.com>
2020-01-03 11:27:03 +0800

    [加入] 從 delivery 頁複製到 pay-method 頁

--- controllers/orderCtrller.js
+++ controllers/orderCtrller.js
@@ -83,8 +83,38 @@ module.exports = {
 
   async payPage(req, res) {
     try {
+      const cartId = req.session.cartId
+      const cart = await Cart.findByPk(cartId, {
+        include: [
+          {
+            association: 'products',
+            attributes: ['id', 'name', 'price'],
+            include: [{
+              association: 'Images',
+              where: { is_main: true }
+            }],
+          }
+        ]
+      })
 
-      res.render('pay')
+      // 確認有無選購商品
+      if (!cart || !cart.products.length) {
+        return res.redirect('/cart')
+      }
+
+      // 製作頁面資料
+      const products = cart.products
+      let totalPrice = 0
+      products.forEach(prod => {
+        prod.quantity = prod.CartItem.quantity
+        prod.amount = (prod.price * prod.quantity)
+        totalPrice += prod.amount
+      })
+
+      // 預設運費
+      const totalPrice2 = totalPrice + 150
+
+      res.render('pay', { css: "delivery", cart, totalPrice, totalPrice2 })
 
     } catch (err) {
       console.error(err)
