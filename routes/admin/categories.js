410d544cd2e0881db2b35c8add538f26bbf12aa5
TomatoSoup <jonathantang0126@gmail.com>
2020-01-05 23:30:05 +0800

    [加入] categories相關路由設定

--- routes/admin/categories.js
+++ routes/admin/categories.js
@@ -3,5 +3,9 @@ const categoryCtrller = require('../../controllers/admin/categoryCtrller')
 
 // route base '/admin/categories'
 router.get('/', categoryCtrller.getCategories)
+router.post('/', categoryCtrller.postCategories)
+router.get('/:categoriesid', categoryCtrller.getEditPage)
+router.put('/:categoriesid', categoryCtrller.putCategory)
+router.delete('/:categoriesid', categoryCtrller.deleteCategory)
 
 module.exports = router
