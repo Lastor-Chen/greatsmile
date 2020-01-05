c475413545fada9b7ca9b414d47c2c7e51ed5be7
TomatoSoup <jonathantang0126@gmail.com>
2020-01-05 23:05:20 +0800

    [加入] GET 分類一覽頁面

--- controllers/admin/categoryCtrller.js
+++ controllers/admin/categoryCtrller.js
@@ -5,10 +5,12 @@ const { Category } = db
 module.exports = {
   getCategories: async (req, res) => {
     try {
-      
-    
 
-    res.render('admin/categories')
+      const categories = await Category.findAll({
+        order: [['id', 'DESC']]
+      })
+
+      res.render('admin/categories', { categories })
     } catch (err) {
       console.error(err)
       res.status(500).json({ status: 'serverError', message: err.toString() })
