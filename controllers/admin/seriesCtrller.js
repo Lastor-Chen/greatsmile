c6e9b964dd8b62b41c2318c6946cacbeb22747de
TomatoSoup <jonathantang0126@gmail.com>
2020-01-06 15:09:15 +0800

    [加入] 作品總覽頁面

--- controllers/admin/seriesCtrller.js
+++ controllers/admin/seriesCtrller.js
@@ -5,10 +5,12 @@ const { Series } = db
 module.exports = {
   getSeries: async (req, res) => {
     try {
+      const series = await Series.findAll({
+        order: [['id', 'DESC']]
+      })
 
-     
-
-      res.render('admin/series')
+      res.render('admin/series', { series})
+    
     } catch (err) {
       console.error(err)
       res.status(500).json({ status: 'serverError', message: err.toString() })
