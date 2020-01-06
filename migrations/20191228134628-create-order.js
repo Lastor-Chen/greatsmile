189a3a7658dc961f0c88d3c98ac417d05ff7b93e
Lastor <lastoraya@gmail.com>
2020-01-06 14:19:19 +0800

    [修改] migration order，錯誤之 schema

--- migrations/20191228134628-create-order.js
+++ migrations/20191228134628-create-order.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
-        type: Sequelize.STRING
+        type: Sequelize.INTEGER
       },
       sn: {
         type: Sequelize.STRING
