c59af46b327cc73a12444349585718f31e54e9fb
Lastor <lastoraya@gmail.com>
2020-01-07 19:23:59 +0800

    [加入] 結帳頁1，前端 form 驗證

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,3 +1,51 @@
+function checkPostCode(elem) {
+  const postCode = $(elem).val()
+  if (isNaN(postCode) || postCode.length !== 5) {
+    elem.setCustomValidity('格式錯誤')
+    $(elem).addClass('is-invalid')
+  } else {
+    elem.setCustomValidity('')
+    $(elem).removeClass('is-invalid')
+    $(elem).addClass('is-valid')
+  }
+}
+
+function checkPhone(elem) {
+  const phone = $(elem).val()
+  if (!/^09\d{2}(-?\d{3}){2}$/.test(phone)) {
+    elem.setCustomValidity('格式錯誤')
+    $(elem).addClass('is-invalid')
+  } else {
+    elem.setCustomValidity('')
+    $(elem).removeClass('is-invalid')
+    $(elem).addClass('is-valid')
+  }
+}
+
+// checkout-1，套用 Bootstrap Form 驗證
+$('.needs-validation').submit(e => {
+  e.preventDefault()
+
+  // setup 自定義驗證
+  checkPostCode($('input[name=postCode]')[0])
+  checkPhone($('input[name=phone]')[0])
+
+  const form = e.target
+  if (!form.checkValidity()) return $(form).addClass('was-validated')
+
+  form.submit()
+})
+
+$('.needs-validation').change(e => {
+  if (e.target.matches('input[name=postCode]')) {
+    checkPostCode(e.target)
+  }
+  
+  if (e.target.matches('input[name=phone]')) {
+    checkPhone(e.target)
+  }
+})
+
 // checkout-2，運費計算
 $('#shipForm').change((e) => {
   const radio = e.target
