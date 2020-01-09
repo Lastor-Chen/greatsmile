e4d3fc2789a61a2b8c8b46bda8d98e97805e363d
TomatoSoup <jonathantang0126@gmail.com>
2020-01-09 12:44:33 +0800

    [優化] 表單驗證優化，增加invalid-feedback區塊

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,5 +1,5 @@
 function checkName(elem) {
-  const isName = $(elem).val().trim() > 0
+  const isName = $(elem).val().trim().length > 0
   if (!isName) {
     elem.setCustomValidity('尚未輸入')
     $(elem).addClass('is-invalid')
@@ -22,14 +22,27 @@ function checkEmail(elem) {
   }
 }
 
+function checkPassword(elem) {
+  const isPassword = $(elem).val().trim().length > 5
+  if (!isPassword) {
+    elem.setCustomValidity('格式錯誤')
+    $(elem).addClass('is-invalid')
+    console.log(isPassword)
+  } else {
+    elem.setCustomValidity('')
+    $(elem).removeClass('is-invalid')
+    $(elem).addClass('is-valid')
+  }
+}
+
 //套用 Bootstrap Form 驗證
 $('.needs-validation').submit(e => {
   e.preventDefault()
 
   // setup 自定義驗證
-
   checkEmail($('input[name=email]')[0])
   checkName($('input[name=lastName]')[0])
+  checkPassword($('input[name=password]')[0])
 
   const form = e.target
   if (!form.checkValidity()) return $(form).addClass('was-validated')
@@ -48,6 +61,10 @@ $('.needs-validation').change(e => {
 
   if (e.target.matches('input[name=email]')) {
     checkEmail(e.target)
+  }
+
+  if (e.target.matches('input[name=password]')) {
+    checkPassword(e.target)
   }
 })
 
