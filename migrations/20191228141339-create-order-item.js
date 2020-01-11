334128f2684607c3d7495ca856a3777dbae864d9
Lastor <lastoraya@gmail.com>
2020-01-11 14:30:55 +0800

    [加入] migration 關聯，當 Order delete 時，刪除被關聯之 OrderItem

--- migrations/20191228141339-create-order-item.js
+++ migrations/20191228141339-create-order-item.js
@@ -15,7 +15,12 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       order_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'orders',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
       },
       product_id: {
         type: Sequelize.INTEGER
