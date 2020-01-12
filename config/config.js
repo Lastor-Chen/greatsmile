94604ab8c13cc118a2a3b115ddbd6ba66d464687
Lastor <lastoraya@gmail.com>
2020-01-12 14:01:08 +0800

    [加入] heroku JAWSDB config

--- config/config.js
+++ config/config.js
@@ -25,11 +25,9 @@ module.exports = {
     "logging": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql",
+    "timezone": '+08:00',
     "define": {
       "underscored": true
     },
