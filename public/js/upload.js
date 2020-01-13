4a1e031f5b8408c06ab29a7b1e087b76b835a454
TomatoSoup <jonathantang0126@gmail.com>
2020-01-13 23:07:09 +0800

    [加入] 編輯欄位圖片預覽

--- public/js/upload.js
+++ public/js/upload.js
@@ -3,12 +3,29 @@ $("#image").change(function () {
 })
 
 
+$(document).on('change', '.inputImg', function () {
+  showEditImg(this)
+})
+
+
 function readImgUrl(input) {
   if (input.files && input.files[0]) {
 
     let reader = new FileReader()
     reader.onload = function (e) {
       $("#preview_img").attr('src', e.target.result)
+    }
+    reader.readAsDataURL(input.files[0])
+
+  }
+}
+
+function showEditImg(input) {
+  if (input.files && input.files[0]) {
+
+    let reader = new FileReader()
+    reader.onload = function (e) {
+      input.nextElementSibling.src =  e.target.result
     }
     reader.readAsDataURL(input.files[0])
 
