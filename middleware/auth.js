84bf105c5b44ca203b211dc2ae653fa02738c9a4
Lastor <lastoraya@gmail.com>
2020-01-13 20:29:55 +0800

    [調整] 未登入結帳時，導入 users/signin/checkout

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,11 @@
 
 module.exports = {
   isAuth: (req, res, next) => {
-    if (!req.isAuthenticated()) return res.redirect('/users/signin')
+    if (!req.isAuthenticated()) {
+      if (req.path === '/checkout') return res.redirect('/users/signin/checkout')
+
+      return res.redirect('/users/signin')
+    }
     next()
   },
 
