bf30234675dfe1618a58f6a45af88f29fcf00ee1
Lastor <lastoraya@gmail.com>
2020-01-13 23:18:34 +0800

    [加入] model Cart，加入 UserId 欄位

--- migrations/20191226032053-create-cart.js
+++ migrations/20191226032053-create-cart.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
