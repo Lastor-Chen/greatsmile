19bd079fa68962f3f0d2530f71814fe5bb8a3ea7
TomatoSoup <jonathantang0126@gmail.com>
2020-01-14 16:58:02 +0800

    [優化] 移除多餘的內容

--- public/js/admin/product.js
+++ public/js/admin/product.js
@@ -1,25 +1,10 @@
 $("#image").change(function () {
-  console.log('yo')
   readImgUrl(this);
 })
 
-// function readImgUrl(input) {
-//   if (input.files && input.files[0]) {
-
-//     let reader = new FileReader()
-//     reader.onload = function (e) {
-//       $("#preview_img").attr('src', e.target.result)
-//     }
-//     reader.readAsDataURL(input.files[0])
-
-//   }
-// }
-
 function readImgUrl(input) {
   if (input.files && input.files.length >= 0) {
-    console.log('in')
     for (let i = 0; i < input.files.length; i++) {
-      console.log(i)
       let reader = new FileReader();
       reader.onload = function (e) {
         let img = `
@@ -29,8 +14,6 @@ function readImgUrl(input) {
           </label>
         </div>
         `
-        // $("<img width='200' height='150'>").attr('src', e.target.result);
-        console.log(img)
         $('#images').append(img);
       }
       reader.readAsDataURL(input.files[i]);
