c8b54af7c94a6697fcefa2f14472737dc3e7f5d0
wendy hsiao <wendy19940331@gmail.com>
2020-01-16 17:29:18 +0800

    [加入] 資料帶入 home 頁

--- controllers/homeCtrller.js
+++ controllers/homeCtrller.js
@@ -1,11 +1,15 @@
 const nodemailer = require('nodemailer');
 const db = require('../models')
 
+const { getCategoryBar } = require('../lib/category.js')
 
 module.exports = {
   async getHome(req, res) {
     try {
+      // 取得 navbar 分類
+      const categoryBar = await getCategoryBar(req)
 
+      res.render('home', { categoryBar})
     } catch (err) {
       console.error(err)
       res.status(500).json({ status: 'serverError', message: err.toString() })
