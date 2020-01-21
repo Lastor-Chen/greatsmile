17b60dac59fb7035f1e63e16b726ba8bee050754
Lastor <lastoraya@gmail.com>
2020-01-21 16:12:09 +0800

    [加入] 1筆 figma seed

--- seeders/20191218123353-series-seeder.js
+++ seeders/20191218123353-series-seeder.js
@@ -3,8 +3,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     const series = [
-      '鬼滅之刃', '中華一番', 'MARVEL', 'Fate/Grand Order ', '新幹線變形機器人 Shinkalion',
-      'Re:從零開始的異世界生活', 'SSSS.GRIDMAN', '來自深淵', '絆愛', '艦隊Collection', '翠星上的加爾岡緹亞'
+      'Re:從零開始的異世界生活', 'SSSS.GRIDMAN', '鬼滅之刃', '來自深淵', 
+      '絆愛', '為美好的世界獻上祝福！', '艦隊Collection', '翠星上的加爾岡緹亞'
     ]
     return queryInterface.bulkInsert('series',
       series.map((item, index) => ({
--- seeders/json/images.js
+++ seeders/json/images.js
@@ -8,38 +8,8 @@ function genImgSeed(productId, imgs) {
 
 const seed = []
 
-// 黏土人 竈門炭治郎
-seed.push(...genImgSeed(101, [
-  "https://lh3.googleusercontent.com/sdEhhph_nkIIigbBOLv7TLK-UAnd641f3t3h6fcg7SZVmeFOHwYP-UBl5OCNcre8xTPE127KCco9wjJWFTKu9_SdWzynGgF1RyX1VJCYeRDZkYC7zZA1_OOSpah7ZVyabxXxHZ2eSyA=w2400",
-]))
-
-// 黏土人 竈門禰豆子
-seed.push(...genImgSeed(102, [
-  "https://lh3.googleusercontent.com/LY7pWJ2JfiwVI6K-qaGAjnJLf53ZAs2pVQgpvxs0FppKhzTRBs_p_-9N16agGT9wVX0IPJ6PJWBxRx1v5btHbdpd8SruQGFBkoU9SPlKoSR5k_IZM1KVk2REKVe1JJLyd93AGyisO34=w2400",
-]))
-
-// 黏土人 劉昴星
-seed.push(...genImgSeed(103, [
-  "https://lh3.googleusercontent.com/dDwOG92o_rCPk_b9-06jXUHiDj4s8z1UuWVfZRiCc7B2dLcDGsAvqXjsX4l6WfY__2ORjJy91jQTcx8MBs6k9y1y-XcVbpSxhqPwdzAek6vaKtK09OMuvLyMVeS3saSYRZ4vJSMkLd4=w2400",
-]))
-
-// 黏土人 鋼鐵人 Mark85 終局之戰Ver. DX
-seed.push(...genImgSeed(104, [
-  "https://lh3.googleusercontent.com/HjYWH6hIAxJ0Z-VS8J5BK1SpnoS4Fh2QCCZkjiVX3cb9rw2G7FsPvTaZsfIS3rJNVq2cPRaU0__DTUVHqijBio1hU_MfOaT2Iy3BgngdeOenWLVbY7MW0cJ9Qvw1jNepIdif3wNARkw=w2400",
-]))
-
-// figma 藤丸立香
-seed.push(...genImgSeed(105, [
-  "https://lh3.googleusercontent.com/9ebaverS7qnoOlRonXqbO74-C26DxE3nO8wm1ZZGLidpI-M5Nj9oPWJauLFiHkyWwUHFNMN5Jd8mY_Cc7RqVjs4w8eOKOP1HNwvA5KBgo_NLt8-fooG8hi3lVJDVaHo-yfTte_gTTuk=w2400",
-]))
-
-// MODEROID Shinkalion 500 TYPE EVA
-seed.push(...genImgSeed(106, [
-  "https://lh3.googleusercontent.com/SSyP1yz5bCQd0ovjSeMHGQxUjFyB8tqqziTiExzFWJqHdB3hEDaXfrWTh38X_FUZtGO2B4uA-Bt0fpeRbYuIgilbrfQ7Cd7Eju2KCym1B5gjcJmhIYHlSWOx1m7vBjGH3tl46oHcprA=w2400",
-]))
-
 // 雷姆 萬聖節Ver.
-seed.push(...genImgSeed(107, [
+seed.push(...genImgSeed(101, [
   'https://i.imgur.com/Wxd4r6G.jpg',
   'https://i.imgur.com/IXu8WQL.jpg',
   'https://i.imgur.com/3i1owFq.jpg',
@@ -53,7 +23,7 @@ seed.push(...genImgSeed(107, [
 ]))
 
 // 寶多六花 ～I believe in future～
-seed.push(...genImgSeed(108, [
+seed.push(...genImgSeed(102, [
   'https://i.imgur.com/QkKeb5i.jpg',
   'https://i.imgur.com/DoEGJDM.jpg',
   'https://i.imgur.com/Sj7Lglh.jpg',
@@ -63,8 +33,18 @@ seed.push(...genImgSeed(108, [
   'https://i.imgur.com/ixHE55x.jpg'
 ]))
 
+// 黏土人 竈門炭治郎
+seed.push(...genImgSeed(103, [
+  "https://lh3.googleusercontent.com/sdEhhph_nkIIigbBOLv7TLK-UAnd641f3t3h6fcg7SZVmeFOHwYP-UBl5OCNcre8xTPE127KCco9wjJWFTKu9_SdWzynGgF1RyX1VJCYeRDZkYC7zZA1_OOSpah7ZVyabxXxHZ2eSyA=w2400",
+]))
+
+// 黏土人 竈門禰豆子
+seed.push(...genImgSeed(104, [
+  "https://lh3.googleusercontent.com/LY7pWJ2JfiwVI6K-qaGAjnJLf53ZAs2pVQgpvxs0FppKhzTRBs_p_-9N16agGT9wVX0IPJ6PJWBxRx1v5btHbdpd8SruQGFBkoU9SPlKoSR5k_IZM1KVk2REKVe1JJLyd93AGyisO34=w2400",
+]))
+
 // 奈奈祈
-seed.push(...genImgSeed(109, [
+seed.push(...genImgSeed(105, [
   'https://i.imgur.com/GcxJ6Fr.jpg',
   'https://i.imgur.com/N5bEyCn.jpg',
   'https://i.imgur.com/W6otWjf.jpg',
@@ -76,7 +56,7 @@ seed.push(...genImgSeed(109, [
 ]))
 
 // 絆愛
-seed.push(...genImgSeed(110, [
+seed.push(...genImgSeed(106, [
   'https://i.imgur.com/ajkFV0G.jpg',
   'https://i.imgur.com/ZoPsNPE.jpg',
   'https://i.imgur.com/GkwdWuR.jpg',
@@ -86,8 +66,20 @@ seed.push(...genImgSeed(110, [
   'https://i.imgur.com/QDYHI1U.jpg'
 ]))
 
+// figma 阿克婭
+seed.push(...genImgSeed(107, [
+  'https://i.imgur.com/Xzxi3zH.jpg',
+  'https://i.imgur.com/E4oFJAa.jpg',
+  'https://i.imgur.com/DJy1iLQ.jpg',
+  'https://i.imgur.com/eYOtp17.jpg',
+  'https://i.imgur.com/JysT9ua.jpg',
+  'https://i.imgur.com/NBM0LSv.jpg',
+  'https://i.imgur.com/vHbNJhK.jpg',
+  'https://i.imgur.com/qhHZLNk.jpg'
+]))
+
 // 大和
-seed.push(...genImgSeed(111, [
+seed.push(...genImgSeed(108, [
   'https://i.imgur.com/S1KTKVb.jpg',
   'https://i.imgur.com/IbtjSLJ.jpg',
   'https://i.imgur.com/Ma0hNZh.jpg',
@@ -95,7 +87,7 @@ seed.push(...genImgSeed(111, [
 ]))
 
 // 翠星
-seed.push(...genImgSeed(112, [
+seed.push(...genImgSeed(109, [
   'https://i.imgur.com/T6SkkN9.jpg',
   'https://i.imgur.com/h2vBysW.jpg',
   'https://i.imgur.com/9o3zpQ5.jpg',
