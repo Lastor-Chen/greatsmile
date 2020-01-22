ca122afca02a1f124a21cf1df321793ce2d7fa0d
TomatoSoup <jonathantang0126@gmail.com>
2020-01-22 22:24:13 +0800

    [加入] 切換出貨狀態路由

--- routes/admin/orders.js
+++ routes/admin/orders.js
@@ -3,6 +3,7 @@ const orderCtrller = require('../../controllers/admin/orderCtrller')
 
 // route base '/admin/orders'
 router.get('/', orderCtrller.getOrders)
+router.post('/:ordersid/ship', orderCtrller.swtichShipStatus)
 router.put('/:ordersid/cancel', orderCtrller.cancelOrder)
 router.put('/:ordersid' , orderCtrller.putOrderInfo)
 
