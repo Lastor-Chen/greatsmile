24171a76b7bc7a9cc74ecc6b95608cbadde1c0aa
Lastor <lastoraya@gmail.com>
2020-02-07 01:25:23 +0800

    [移除] test 路由

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,6 @@ const { isAuth } = require('../middleware/auth.js')
 const { getCategoryBar } = require('../middleware/category.js')
 const { getTagGroup } = require('../middleware/tag.js')
 
-const db = require('../models')
-const Op = db.Sequelize.Op
-const { Product } = db
-const { checkInv, txnOrder } = require('../lib/order_helper.js')
-
 module.exports = app => {
   app.use('/admin', require('./admin/index.js'))
   app.post('/newebpay/callback', newebpayCb)  // 金流API callback
@@ -28,36 +23,4 @@ module.exports = app => {
 
   app.get('/', (req, res) => res.redirect('/products'))
   app.get('/search', getTagGroup, require('../controllers/prodCtrller').getProducts)
-
-  app.get('/test', async (req, res) => {
-    const cartProds = [
-      { id: 1, CartItem: { quantity: 1 }},
-      // { id: 2, CartItem: { quantity: 1 }},
-      { id: 3, CartItem: { quantity: 1 }},
-    ]
-    const queryArray = cartProds.map(prod => ({ id: prod.id }))
-
-    try {
-      // 檢查商品庫存
-      const products = await Product.findAll(
-        { where: { [Op.or]: queryArray } }
-      )
-
-      const error = checkInv(products, cartProds)
-      if (error) {
-        req.flash('error', error)
-        return res.redirect('/cart')
-      }
-      
-      // 成立訂單 by transaction
-      const txnError = await txnOrder()
-      if (txnError) { throw txnError }
-
-      res.send('fin')
-    } catch (error) {
-      console.log(error.toString())
-      res.send('error')
-    }
-  })
 }
-
