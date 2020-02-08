060b7797bc6181eea77019bdc2cfda5ee36c457e
Lastor <lastoraya@gmail.com>
2020-02-08 15:02:10 +0800

    [移除] 預約中、發售中，Tag 種子資料

--- seeders/20191218125001-tag-seeder.js
+++ seeders/20191218125001-tag-seeder.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    const tag = ['預約中', '附特典', '庫存發售中']
+    const tag = ['附特典']
     return queryInterface.bulkInsert('tags',
       tag.map((item, index) => ({
         name: item,
--- seeders/20191218130133-tagItem-seeder.js
+++ seeders/20191218130133-tagItem-seeder.js
@@ -6,21 +6,9 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
       queryInterface.bulkInsert('tag_items',
-        Array.from({ length: 50 }, (val, index) => ({
-          tag_Id: 1,  // 預約中
-          product_id: index + 51
-        }))
-      ),
-      queryInterface.bulkInsert('tag_items',
         Array.from({ length: 30 }, (val, index) => ({
-          tag_Id: 2,  // 附特典
-          product_id: (index + 1) * 3
-        }))
-      ),
-      queryInterface.bulkInsert('tag_items',
-        Array.from({ length: 50 }, (val, index) => ({
-          tag_Id: 3,  // 發售中
-          product_id: index + 1
+          tag_Id: 1,  // 附特典
+          product_id: (index + 1) * 3  // 每 3 筆賦予特典
         }))
       ),
       queryInterface.bulkInsert('tag_items', tagItems)
@@ -34,11 +22,3 @@ module.exports = {
     ])
   }
 };
-
-/**
- * 共 100 筆
- * product_id   tag
- *   51-100     1 (預約中)
- *   3n         2 (附特典)
- *   1-50       3 (發售中)
- */
