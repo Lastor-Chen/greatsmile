36348b235404145dbe6f988476a08d11054db130
Lastor <lastoraya@gmail.com>
2023-07-30 00:57:53 +0800

    [加入] fly.io deploy setting

--- config/config.js
+++ config/config.js
@@ -24,7 +24,10 @@ module.exports = {
     "logging": false
   },
   "production": {
-    "use_env_variable": "JAWSDB_URL",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql",
     "timezone": '+08:00',
     "define": {
